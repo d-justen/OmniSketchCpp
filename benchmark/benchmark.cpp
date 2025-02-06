19bf1c71ae66ae22f54e8729eacb83c9d62eb9b4
d-justen <david.justen@tu-berlin.de>
2025-02-06 12:42:48 +0100

    add benchmark

--- benchmark/benchmark.cpp
+++ benchmark/benchmark.cpp
@@ -53,10 +53,33 @@ static void BM_OmniSketchIntersect(benchmark::State &state) {
 	std::vector<size_t> search_keys(keys.begin(), std::next(keys.begin(), 1024));
 
 	for (auto _ : state) {
-		auto card = sketch.EstimateCardinality(search_keys.data(), 1024);
+		for (size_t i = 0; i < 1024; i++) {
+			auto card = sketch.EstimateCardinality(search_keys[i]);
+		}
 	}
 }
 
 BENCHMARK(BM_OmniSketchIntersect);
+
+static void BM_OmniSketchIntersectBatched(benchmark::State &state) {
+	std::vector<size_t> keys(6000000);
+	std::vector<size_t> rids(6000000);
+
+	for (size_t i = 0; i < 6000000; i++) {
+		keys[i] = i % 50000;
+		rids[i] = i;
+	}
+
+	omnisketch::OmniSketch sketch(4096, 3, 8);
+	sketch.InitializeBuffers(6000000);
+	sketch.AddRecords(keys.data(), rids.data(), 6000000);
+	std::vector<size_t> search_keys(keys.begin(), std::next(keys.begin(), 1024));
+
+	for (auto _ : state) {
+		auto card = sketch.EstimateCardinality(search_keys.data(), 1024);
+	}
+}
+
+BENCHMARK(BM_OmniSketchIntersectBatched);
 
 BENCHMARK_MAIN();
